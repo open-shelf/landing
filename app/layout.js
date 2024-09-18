2c0d6b76f6bdfa6f8495b3c134f63eb38f99e27d
santhosh <santosh.kumar@zenotta.com>
2024-09-18 23:35:51 +0200

    added support for amplitude

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "OpenShelf",
@@ -9,6 +10,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://cdn.amplitude.com/libs/analytics-browser-2.11.1-min.js.gz"
+          strategy="beforeInteractive"
+        />
+        <Script
+          src="https://cdn.amplitude.com/libs/plugin-session-replay-browser-1.6.22-min.js.gz"
+          strategy="beforeInteractive"
+        />
+        <Script id="amplitude-init" strategy="afterInteractive">
+          {`
+            window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1}));
+            window.amplitude.init('2c7ccf63efcf1551b707688f9117645', {"serverZone":"EU","autocapture":{"elementInteractions":true}});
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
